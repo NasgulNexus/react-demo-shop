3acfa11fe6952613ca61c56ffe090fc54b638c4a
NasgulNexus <moctegor@gmail.com>
2022-01-25 14:23:06 +0300

    gh-pages

--- src/actions/config.js
+++
@@ -1,3 +0,0 @@
-export const domain = "https://fakestoreapi.com";
-export const productUrl = "/products";
-export const limit = 20;
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-
-import rootReducer from "./reducers";
-
-export const create = () => {
-  const store = createStore(rootReducer, applyMiddleware(thunk));
-  return store;
-};
